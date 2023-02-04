06d2abda324f0e45178f24004c963df219fa2b37
sehqlr <hey@samhatfield.me>
2023-02-04 06:01:05 +0000

    Remove socket mode

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { App } = require('@slack/bolt');
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
     appToken: process.env.SLACK_APP_TOKEN,
     port: process.env.PORT || 3000
 });
