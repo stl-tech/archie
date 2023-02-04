8dbab2d79b6f597fa00518a48653c62e0a3eeeb5
sehqlr <hey@samhatfield.me>
2023-02-04 04:06:25 +0000

    Got admin message command working

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ app.message('hello', async ({ message, say }) => {
     await say(`Hey there <@${message.user}>!`);
 });
 
+app.command('/admin', async ({ command, ack, client, respond, logger }) => {
+    await ack();
+
+    try {
+        await client.chat.postMessage({
+            text: `Incoming message for admins from <@${command.user_name}> in <@${command.channel_name}>:\n${command.text}`,
+            channel: "admin"
+        });
+        await respond(`The admins have been messaged, <@${command.user_name}>`);
+    }
+    catch (error) {
+        await respond(`Something went wrong!\n${error}`);
+    }
+})
+
 app.start()
